4d03165dac3e6b50c61eb6e1425368775fa7c720
RockRoofing <james@rockroofing.co.uk>
2026-08-17 04:39:29 +0100

    340

--- pages/scorecard-crm.js
+++ pages/scorecard-crm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import Head from 'next/head'
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
 import PreContractNav from '../components/PreContractNav'
